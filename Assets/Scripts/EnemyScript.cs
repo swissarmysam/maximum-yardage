ce4662c2339f55aca1fb616a3cc4e7ba6b1b9fc6
swissarmysam <srampling90@gmail.com>
2020-03-08 21:00:43 +0000

    EnemyScript Added

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    // Set sprites for the two action states that enemy can have
+    // public Sprite enemyJump;
+    // public Sprite enemySlide;
+
+    public float enemyMaxMovementSpeed = 2.0f;
+    public float enemyJumpForce = 60.0f;
+    
+    public float actionInterval = 0.5f;
+    private bool isJumping = false;
+    private bool isSliding = false;
+    private float timeUntilNextAction;
+
+    private Collider2D enemyCollider;
+    //private SpriteRenderer enemyRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
