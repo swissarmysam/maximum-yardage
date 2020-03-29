6334e849444f40756bc52b9d1d1e6b86ae9c1f40
swissarmysam <srampling90@gmail.com>
2020-03-29 17:24:30 +0100

    Added basic enemy type, end game UI elements and other code fixes

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -9,26 +9,37 @@ public class EnemyScript : MonoBehaviour
     // public Sprite enemyJump;
     // public Sprite enemySlide;
 
-    public float enemyMaxMovementSpeed = 2.0f;
-    public float enemyJumpForce = 60.0f;
+    public float enemyMinSpeed = 2.0f;
+    public float enemyMaxSpeed = 6.0f;
+    public float enemySpeed;
+    public float enemyJumpForce = 20.0f;
     
     public float actionInterval = 0.5f;
     private bool isJumping = false;
     private bool isSliding = false;
     private float timeUntilNextAction;
 
+    public Sprite enemySprite;
     private Collider2D enemyCollider;
-    //private SpriteRenderer enemyRenderer;
+    private SpriteRenderer enemyRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        // references to collider and renderer so adjustments can be made to properties whilst active
+        enemyCollider = gameObject.GetComponent<Collider2D>();
+        enemyRenderer = gameObject.GetComponent<SpriteRenderer>();
+        // set speed for each instantiated enemy object
+        enemySpeed = Random.Range(enemyMinSpeed, enemyMaxSpeed); 
+        Debug.Log(enemySpeed);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // make enemy run towards player at random speed
+        gameObject.transform.position += -transform.right * enemySpeed * Time.deltaTime;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -19,13 +21,20 @@ public class PlayerController : MonoBehaviour
     private Animator playerAnimator;
 
     // bool var for stunned status
-    // private bool isStunned = false;
-
-    // set bool flag for limiting jump action
-    public bool jumpLimit = false;
+    public bool isStunned = false;
 
     // scrolling background
     public ParallaxScroll parallax;
+
+    // variables to track and display score
+    private float yardsRun = 0;
+    private float multiplier = 2.0f;
+    public Text yardsRunLabel;
+
+    // get buttons to restart game after stunned and return to main menu
+    public Button restartGameBtn;
+    public Button mainMenuBtn;
+    public Button quitBtn;
 
     // Start is called before the first frame update
     void Start()
@@ -41,6 +50,8 @@ public class PlayerController : MonoBehaviour
 
         // see if jump action was made
         bool jumpActive = Input.GetButton("Fire1");
+        // prevent jump action if player is stunned
+        jumpActive = jumpActive && !isStunned;
 
         // jump is only registered if the player isGrounded - prevents jump from being held down
         if (jumpActive && isGrounded)
@@ -52,14 +63,31 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        Vector2 newVelocity = playerRigidBody.velocity;
-        newVelocity.x = forwardSpeed;
-        playerRigidBody.velocity = newVelocity;
+        // if the player is not stunned then move forward
+        if(!isStunned)
+        {
+            Vector2 newVelocity = playerRigidBody.velocity;
+            newVelocity.x = forwardSpeed;
+            playerRigidBody.velocity = newVelocity;
+
+            // update yardsRun
+            yardsRun += multiplier * Time.deltaTime;
+            // update text and limit to one decimal point on display
+            yardsRunLabel.text = yardsRun.ToString("F1");
+            // Debug.Log(yardsRun);
+        }
 
         // keep checking if player is grounded
         UpdateGroundedStatus();
 
         parallax.offset = transform.position.x;
+
+        if(isStunned && isGrounded)
+        {
+            restartGameBtn.gameObject.SetActive(true);
+            mainMenuBtn.gameObject.SetActive(true);
+            quitBtn.gameObject.SetActive(true);
+        }
     }
 
     void UpdateGroundedStatus()
@@ -68,6 +96,32 @@ public class PlayerController : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
         // this updates the animator isGrounded parameter
         playerAnimator.SetBool("isGrounded", isGrounded);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        HitByEnemy(collider);
+    }
+
+    void HitByEnemy(Collider2D enemyCollider)
+    {
+        isStunned = true;
+        playerAnimator.SetBool("isStunned", true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
 }
